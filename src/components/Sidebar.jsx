540c14d1c04d0fd58ded12a28a9b5bcde70cffe0
poponman <yuripon1221@gmail.com>
2025-05-28 12:34:14 +0900

    Sidebarコンポーネントの追加

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Sidebar = () => {
+  return (
+    <div className = " Sidebar">
+      <ul className="SidebarList">
+        <li></li>
+      </ul>
+    </div>
+  )
+}
