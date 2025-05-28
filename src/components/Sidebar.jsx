9055dffc1a44f464e408097bb3414e82cdd8ece7
poponman <yuripon1221@gmail.com>
2025-05-28 13:10:43 +0900

    SidebarDataから取得したデータを画面に表示、クリック時の操作追加

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
+import { SidebarData } from './SidebarData'
 
 export const Sidebar = () => {
   return (
     <div className = " Sidebar">
       <ul className="SidebarList">
-        <li></li>
+        {SidebarData.map((value, key) => {
+          return(
+            <li
+              key = {key}
+              classNamw="row"
+              onClick={()=>{
+                window.location.pathname =value.link;
+              }}
+            >
+              <div id = "icon">{value.icon}</div>
+              <div id = "title">{value.title}</div>
+            </li>
+          )
+        })}
       </ul>
     </div>
   )
